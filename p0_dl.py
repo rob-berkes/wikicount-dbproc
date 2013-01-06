9d7202a9e9ebfa490dcab1f1a0c13e18947c11a1
EC2 Default User <ec2-user@ip-10-195-138-15.ec2.internal>
2013-01-06 03:49:59 +0000

    clean up, remove unneeded includes and other statements

--- p0_dl.py
+++ p0_dl.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
-from datetime import date
-from pymongo import Connection
-import time
 import urllib2
-import gzip
-import hashlib
-import random
-import re
 import os
 from functions import wikicount
+import syslog
 
 FILEBASE="/tmp/staging/pagecounts.tmp"
 URL="http://dumps.wikimedia.org/other/pagecounts-raw/2012/2012-11/pagecounts-"
@@ -17,25 +11,18 @@ DAY,MONTH,YEAR,HOUR,expiretime=wikicount.fnReturnTimes()
 HOUR=int(HOUR)-1
 DAY,MONTH,HOUR=wikicount.fnFormatTimes(DAY,MONTH,HOUR)
 URL="http://dumps.wikimedia.org/other/pagecounts-raw/"+str(YEAR)+"/"+str(YEAR)+"-"+str(MONTH)+"/pagecounts-"
-print HOUR
+#print HOUR
 URLDATE=str(YEAR)+str(MONTH)+str(DAY)
 URLSUFFIX="-"+str(HOUR)+"0000.gz"
 URL+=URLDATE
 URL+=URLSUFFIX
-HITS=0
-FILEPROC2="/tmp/staging/q2_pagecounts.processed."+str(HOUR)
-FILEPROC3="/tmp/staging/q3_pagecounts.processed."+str(HOUR)
-FILEPROC4="/tmp/staging/q4_pagecounts.processed."+str(HOUR)
-FILEPROC5="/tmp/staging/q5_pagecounts.processed."+str(HOUR)
-FILEPROC6="/tmp/staging/q6_pagecounts.processed."+str(HOUR)
-FILEPROC7="/tmp/staging/q7_pagecounts.processed."+str(HOUR)
 
 
 #Now with URL, download file
-print "Downloading file: "+URL+" ... "
+syslog.syslog("[p0-dl.py] - starting download"+str(URL))
 COUNTFILE=urllib2.urlopen(URL)
 OFILE=open(FILEBASE,"w")
 OFILE.write(COUNTFILE.read())
 OFILE.close()
-print "Done! Now processing the file..."
+syslog.syslog("[p0-dl.py] - finished download"+str(URL))
 
