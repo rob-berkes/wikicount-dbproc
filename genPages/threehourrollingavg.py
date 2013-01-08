b6b2afcc70179ed8088151c9182ee4569b05c837
Rob Berkes <rob.berkes@gmail.com>
2013-01-08 02:51:34 +0000

    added id field

--- genPages/threehourrollingavg.py
+++ genPages/threehourrollingavg.py
@@ -10,10 +10,10 @@ DAY,MONTH,YEAR,HOUR,expiretimes = wikicount.fnReturnTimes()
 HOUR=int(HOUR)-1
 if HOUR==-1:
 	HOUR=23
-ALLRES=db.hitshourly.find({str(HOUR):{'$gt':15}}).sort(str(HOUR),-1).limit(50000)
+print HOUR
+HOUR=1
 HOUR2=HOUR-1
 HOUR3=HOUR2-1
-
 if HOUR2==-1:
 	HOUR2=23
 if HOUR3==-1:
@@ -21,26 +21,30 @@ if HOUR3==-1:
 elif HOUR3==-2:
 	HOUR3=22
 
+HOUR='%02d' % (HOUR,)
+HOUR2='%02d' % (HOUR2,)
+HOUR3='%02d' % (HOUR3,)
+ALLRES=db.hitshourly.find({str(HOUR):{'$gt':15}}).sort(str(HOUR),-1).limit(50000)
 hourlies=[]
 
 for item in ALLRES:
 	title,utitle=wikicount.MapQuery_FindName(item['_id'])
 	try:
-		b1=item['14']
+		b1=item[HOUR]
 	except KeyError:
 		b1=0
 	try:
-		b2=item['15']
+		b2=item[HOUR2]
 	except KeyError:
 		b2=0
 	try:
-		b3=item['16']
+		b3=item[HOUR3]
 	except KeyError:
 		b3=0
 	
 	rollingavg=mean(array([b1,b2,b3]))
 
-	rec={'title':title,'rollavg':int(rollingavg)}
+	rec={'title':title,'rollavg':int(rollingavg),'id':item['_id']}
 	hourlies.append(rec)
 
 #for w in sorted(hourlies,key=itemgetter('rollavg'),reverse=True):
@@ -50,6 +54,6 @@ z=1
 db.threehour.remove()
 for w in sorted(hourlies,key=itemgetter('rollavg'),reverse=True):
 	if z < 101:
-		rec={'place':z,'title':w['title'],'rollavg':w['rollavg']}
+		rec={'place':z,'title':w['title'],'rollavg':w['rollavg'],'id':w['id']}
 		db.threehour.insert(rec)
 		z+=1
