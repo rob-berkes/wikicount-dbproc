e17a79269510b71095448c73b12727a9938754fd
Rob Berkes <rob.berkes@gmail.com>
2013-01-11 14:39:56 +0000

    mod to add title to insert

--- tophits.py
+++ tophits.py
@@ -10,7 +10,7 @@ RESULT=db.hits.find().sort('Hits',-1).limit(250000)
 
 db.tophits.remove({'d':DAY,'m':MONTH,'y':YEAR})
 for item in RESULT:
-        db.tophits.insert({'id':str(item['_id']),'d':DAY,'m':MONTH,'y':YEAR,'place':RECCOUNT,'Hits':int(item['Hits'])},safe=True)
+        db.tophits.insert({'id':str(item['_id']),'d':DAY,'m':MONTH,'y':YEAR,'place':RECCOUNT,'Hits':int(item['Hits']),'title':item['title']},safe=True)
 	key=str(YEAR)+'_'+str(MONTH)+'_'+str(DAY)
         db.dailytop.update({'_id':str(item['_id'])},
                            {
