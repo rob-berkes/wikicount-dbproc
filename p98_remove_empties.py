8a2b06dcb23b97b117a4549c2c397bf321cc02d2
Rob Berkes <rob.berkes@gmail.com>
2013-01-11 14:33:29 +0000

    modified p98 to remove map record after updating hits table. wont run until templates modified

--- p98_remove_empties.py
+++ p98_remove_empties.py
@@ -3,6 +3,7 @@ conn=Connection()
 db=conn.wc
 for item in db.map.find():
      if db.hits.find_one({'_id':item['_id']}):
-             pass
-     else:
-             db.map.remove({'_id':item['_id']})
+	     TITLESTRING=item['title'].decode('utf-8')
+	     POSTFIND={'_id': item['_id']}
+             db.hits.update(POSTFIND,{"$set":{'title':TITLESTRING} },upsert=True)
+	     db.map.remove(POSTFIND)
