3e1d234e23d031efa39e98922f6f9a60ddfde2d6
Rob Berkes <rob.berkes@gmail.com>
2013-01-12 23:08:21 +0000

    fixed threehour script and added to cron

--- genPages/threehourrollingavg.py
+++ genPages/threehourrollingavg.py
@@ -2,7 +2,7 @@ from pymongo import Connection
 from functions import wikicount
 from numpy import sqrt,mean,array
 from operator import itemgetter
-
+import syslog
 conn=Connection()
 db=conn.wc
 
@@ -26,30 +26,37 @@ HOUR2='%02d' % (HOUR2,)
 HOUR3='%02d' % (HOUR3,)
 ALLRES=db.hitshourly.find({str(HOUR):{'$gt':15}}).sort(str(HOUR),-1).limit(50000)
 hourlies=[]
-
+TypeErrors=0
+KeyErrors=0
 for item in ALLRES:
-	title,utitle=wikicount.MapQuery_FindName(item['_id'])
 	try:
-		b1=item[HOUR]
-	except KeyError:
-		b1=0
-	try:
-		b2=item[HOUR2]
-	except KeyError:
-		b2=0
-	try:
-		b3=item[HOUR3]
-	except KeyError:
-		b3=0
+		QUERYtitle=db.hits.find_one({'_id':item['_id']})
+		atitle=QUERYtitle['title']
+		title,utitle=wikicount.FormatName(atitle)
+		try:
+			b1=item[HOUR]
+		except KeyError:
+			b1=0
+		try:
+			b2=item[HOUR2]
+		except KeyError:
+			b2=0
+		try:
+			b3=item[HOUR3]
+		except KeyError:
+			b3=0
 	
-	rollingavg=mean(array([b1,b2,b3]))
+		rollingavg=mean(array([b1,b2,b3]))
 
-	rec={'title':title,'rollavg':int(rollingavg),'id':item['_id']}
-	hourlies.append(rec)
+		rec={'title':title,'rollavg':int(rollingavg),'id':item['_id']}
+		hourlies.append(rec)
+	except TypeError:
+		TypeErrors+=1
+	except KeyError:
+		KeyErrors+=1
 
-#for w in sorted(hourlies,key=itemgetter('rollavg'),reverse=True):
-#	if w['title']:
-#		print w['title'],w['rollavg']
+syslog.syslog("[3hrrollavg] - TypeErrors: "+str(TypeErrors)+" KeyErrors: "+str(KeyErrors))
+
 z=1
 db.threehour.remove()
 for w in sorted(hourlies,key=itemgetter('rollavg'),reverse=True):
