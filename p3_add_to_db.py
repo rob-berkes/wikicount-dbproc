fcc500f1e379e7d9ab0070fcf63e82bea2575489
Rob Berkes <rob.berkes@gmail.com>
2013-02-05 23:39:18 +0000

    added hitsdaily table, hits table now alltime

--- p3_add_to_db.py
+++ p3_add_to_db.py
@@ -11,8 +11,7 @@ import os
 DAY,MONTH,YEAR,HOUR,expiretime=wikicount.fnReturnTimes()
 HOUR=wikicount.minusHour(int(HOUR))
 DAY,MONTH,HOUR=wikicount.fnFormatTimes(DAY,MONTH,HOUR)
-
-def f(FILEPROC,HOUR):
+def f(FILEPROC,HOUR,DAY,MONTH,YEAR):
      connection=Connection()
      db=connection.wc
      RECORDS=0
@@ -25,11 +24,13 @@ def f(FILEPROC,HOUR):
 		          record=line.strip().split()
 		          HASH=hashlib.sha1(record[1]).hexdigest()
 		          POSTFIND={'_id': HASH}
+			  POSTFINDDAY={'id': HASH,'d':DAY,'m':MONTH,'y':YEAR}
 			  POSTDATE=time.strftime("%D_%H")
 			  TITLESTRING=record[1].decode('utf-8')
 			  TITLEPOST={'_id':HASH,'title':TITLESTRING}
 		          db.hits.update(POSTFIND,{ "$inc" : { "Hits" : int(record[2]) },"$set":{'title':TITLESTRING} },upsert=True)
 			  db.hitshourly.update(POSTFIND,{"$inc":{HOUR:int(record[2])}},upsert=True)
+			  db.hitsdaily.update(POSTFINDDAY,{"$inc":{"Hits": int(record[2])}},upsert=True)
 		  	  #db.map.update(TITLEPOST,upsert=True)
 		          RECORDS+=1
 			except UnicodeDecodeError: 
@@ -64,13 +65,13 @@ FILEPROC8="/tmp/action/q8_pagecounts.processed."+str(HOUR)
 
 
 if __name__ == '__main__':
-    p = Process(target=f, args=(FILEPROC2,HOUR))
-    q = Process(target=f, args=(FILEPROC3,HOUR))
-    r = Process(target=f, args=(FILEPROC4,HOUR))
-    s = Process(target=f, args=(FILEPROC5,HOUR))
-    t = Process(target=f, args=(FILEPROC6,HOUR))
-    u = Process(target=f, args=(FILEPROC7,HOUR))
-    v = Process(target=f, args=(FILEPROC8,HOUR))
+    p = Process(target=f, args=(FILEPROC2,HOUR,DAY,MONTH,YEAR))
+    q = Process(target=f, args=(FILEPROC3,HOUR,DAY,MONTH,YEAR))
+    r = Process(target=f, args=(FILEPROC4,HOUR,DAY,MONTH,YEAR))
+    s = Process(target=f, args=(FILEPROC5,HOUR,DAY,MONTH,YEAR))
+    t = Process(target=f, args=(FILEPROC6,HOUR,DAY,MONTH,YEAR))
+    u = Process(target=f, args=(FILEPROC7,HOUR,DAY,MONTH,YEAR))
+    v = Process(target=f, args=(FILEPROC8,HOUR,DAY,MONTH,YEAR))
 #    w = Process(target=f, args=(FILEPROC9,))
     p.daemon=True
     q.daemon=True
