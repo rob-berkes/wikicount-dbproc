201c5721503e621bd52c55857dffc36287f058c0
Rob Berkes <rob.berkes@gmail.com>
2013-06-21 10:26:20 -0500

    fixes to popcold

--- genPages/populate_cold.py
+++ genPages/populate_cold.py
@@ -1,52 +1,29 @@
 from pymongo import Connection
-from datetime import date
 from multiprocessing import Process
 from functions import wikicount
-import syslog
-import string
 import urllib2
-RECORDSPERPAGE=100
+def cold():
+    COLD_LIST_QUERY = db.tmpHot.find().sort('delta', 1).limit(100)
+    for p in COLD_LIST_QUERY:
+        NAMEQ = db.hitsdaily.findOne({'_id': p['_id']})
+        rec = {'title': NAMEQ['title'], 'place': p['orPlace'], 'Hits': p['delta'], 'linktitle': NAMEQ['title'],
+               '_id': p['_id']}
+        db.prodcold.insert(rec)
 
-def cold(d,m):
-  	COLD_LIST_QUERY=db.tmpHot.find().sort('delta',1).limit(100)
-        title=''
-        for p in COLD_LIST_QUERY:
-                NAMEQ=db.hitsdaily.findOne({'_id':p['_id']})
-                rec={'title':NAMEQ['title'],'place':p['orPlace'],'Hits':p['delta'],'linktitle':NAMEQ['title'],'_id':p['_id']}
-                db.prodcold.insert(rec)
-	
         return
-STARTTIME=wikicount.fnStartTimer()
-syslog.syslog('populate_cold:  starting...')
-DAY,MONTH,YEAR,HOUR,expiretime=wikicount.fnReturnTimes()
-HOUR=wikicount.minusHour(int(HOUR))
-MONTHNAME=wikicount.fnGetMonthName()
-COLLECTIONNAME=str(YEAR)+MONTHNAME
-d=DAY
-yd=int(DAY)-1
-if yd==0:
-        yd=30
-m=MONTH
-ym=MONTH
-if yd==30:
-        ym=int(m)-1
-y=YEAR
-
-conn=Connection()
-db=conn.wc
-RECCOUNT=1
-NUMRECS=62500
-debutCount=0
-wikicount.fnSetStatusMsg('populate_cold',0)
+STARTTIME = wikicount.fnStartTimer()
+wikicount.syslog('populate_cold:  starting...')
+DAY, MONTH, YEAR, HOUR, expiretime = wikicount.fnReturnTimes()
+conn = Connection()
+db = conn.wc
+wikicount.fnSetStatusMsg('populate_cold', 0)
 db.prodcold.remove()
 
-p = Process(target=cold, args=(d,m))
-
+p = Process(target=cold, args=())
 p.start()
-
 p.join()
 
-RUNTIME=wikicount.fnEndTimerCalcRuntime(STARTTIME)
-syslog.syslog('populate_cold: runtime is '+str(RUNTIME)+' seconds.')
-wikicount.fnSetStatusMsg('populate_cold',3)
+RUNTIME = wikicount.fnEndTimerCalcRuntime(STARTTIME)
+wikicount.syslog('populate_cold: runtime is ' + str(RUNTIME) + ' seconds.')
+wikicount.fnSetStatusMsg('populate_cold', 3)
 wikicount.fnLaunchNextJob('populate_cold')
