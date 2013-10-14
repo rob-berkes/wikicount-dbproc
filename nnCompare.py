75ccb7b3bff1f649affadc85ca8dbd5008d76aca
Rob Berkes <rob.berkes@gmail.com>
2013-10-14 06:25:20 +0000

    going to bed...

--- nnCompare.py
+++ nnCompare.py
@@ -14,7 +14,7 @@ def printHourlyDifferences():
 		initVALUES=initRS['Values']
 		print "Showing differences in hourly weights since last run and all time..."
 		for a in range(0,23):
-			print 'Hour: '+str(a)+' Diff Change: '+str(float(VALUES[a])-oldVALUES[a])+'   Total: '+str(float(VALUES[a])-initVALUES[a])
+			print 'Hour: %d Diff Change: %.6f Total: %.6f' % (a,float(VALUES[a])-oldVALUES[a],float(VALUES[a])-initVALUES[a])
 	except TypeError:
 		print 'Old values do not exist. Transferring current values over. Please rerun after doing some more scoring'
 		NEWREC={'_id':'InitWikiHourlyWeights','Values':VALUES}
@@ -29,14 +29,12 @@ def printDayDifferences():
 	RS=db['settings'].find_one({'_id':'WikiDayWeights'})
 	
 	VALUES=RS['Values']
-	try:
-		oldVALUES=oldRS['Values']
-		initVALUES=initRS['Values']
-		print "Displaying differences in daily weights since last backup and in total."
-		for a in range(1,15):
-			print 'Day: '+str(a)+' Diff Change: '+str(float(VALUES[a])-oldVALUES[a])+'    Total: '+str(float(VALUES[a])-initVALUES[a])
-	except TypeError:
-		print "Old values appear not to exist.  Please rerun after doing some more scoring, it should then work"
+#	try:
+	oldVALUES=oldRS['Values']
+	initVALUES=initRS['Values']
+	print "Displaying differences in daily weights since last backup and in total."
+	for a in range(1,15):
+		print 'Day: %d Diff Change: %.6f Total: %.6f' % (a,float(VALUES[a])-oldVALUES[a],float(VALUES[a])-initVALUES[a])
 
 
 	#NOW make cur values 'old' values
