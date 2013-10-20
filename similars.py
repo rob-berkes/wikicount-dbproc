89bf52b2268b31990aa41cda5d6f49dd6005e986
Rob Berkes <rob.berkes@gmail.com>
2013-10-20 01:54:46 +0000

    similars-nn

--- similars.py
+++ similars.py
@@ -1,7 +1,9 @@
 from pymongo import Connection
+from datetime import time
 
 class Hour:
 	hour=0
+	hits=0
 	h2nratio=0
 	hourscore=0
 	hm1score=0
@@ -13,10 +15,28 @@ class Hour:
 		hm1score=float(0)
 		hp1score=float(0)
 	def calcratio(DIVISOR):
-		return float(HOUR/DIVISOR)
+		return float(hits/DIVISOR)
 	def calcscore(SCORE,DIVISOR):
 		return float(1/(SCORE-DIVISOR))
 
 conn=Connection()
 db=conn.wc
 
+def getAllHourHits(IDREC):
+	DAY=[]
+	rs=db['en_hitshourly'].find_one({'_id':IDREC})
+	for hour in range(0,24):
+		a=Hour()
+		a.hour=hour
+		HOURSTR="%02d" %(hour,)
+		try:
+			a.hits=rs[HOURSTR]
+		except KeyError:
+			a.hits=0
+		DAY.append(a)
+	return DAY
+
+TODAY=getAllHourHits('a2976cc2ec4ddbd09e87da88f65b6df552d850eb')
+
+for hour in TODAY:
+	print str(hour.hour)+" : "+str(hour.hits)
